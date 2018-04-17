23b10ab4480de809c836ba7dd23367590f68179d
Riet,Redmar R.F. van de <r.vanderiet@student.fontys.nl>
2018-04-17 17:48:25 +0200

    fonts

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -1,14 +1,17 @@
-//#include "SDL.h"
+#include "SDL.h"
 #include "SDL_image.h"
+#include "SDL_ttf.h"
 
 #include "stdio.h"
-#include "string"
+#include <string>
+#include <cmath>
 
-#define SCREEN_HEIGHT 800
-#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 640
+#define SCREEN_WIDTH 480
 
 SDL_Window *screen = NULL;
 SDL_Renderer * sdlRenderer = NULL;
+TTF_Font *gFont = NULL;
 
 class LTexture
 {
@@ -18,6 +21,8 @@ class LTexture
 
         bool loadFromFile( std::string path);
 
+        bool loadFromRenderedText( std::string textureText, SDL_Color textColor);
+
         void free();
 
         void setColor( Uint8 red, Uint8 green, Uint8 blue);
@@ -26,7 +31,7 @@ class LTexture
 
         void setAlpha( Uint8 alpha);
 
-        void render(int x, int y, SDL_Rect* clip = NULL);
+        void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
         int getWidth();
         int getHeight();
@@ -75,6 +80,28 @@ bool LTexture::loadFromFile( std::string path)
     return mTexture != NULL;
 }
 
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor){
+    free();
+    SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor);
+    if (textSurface == NULL)
+    {
+        printf_s("Unable to render text surface. Error: %s\n", TTF_GetError());
+    } else {
+        mTexture = SDL_CreateTextureFromSurface(sdlRenderer, textSurface);
+        if (mTexture == NULL)
+        {
+            printf_s("Unable to create texture from text, Error %s\n", SDL_GetError());
+        } else {
+            mWidth = textSurface->w;
+            mHeight = textSurface->h;
+        }
+
+        SDL_FreeSurface(textSurface);
+    }
+
+    return mTexture != NULL;
+}
+
 void LTexture::free(){
     if (mTexture != NULL)
     {
@@ -97,14 +124,14 @@ void LTexture::setAlpha(Uint8 alpha){
     SDL_SetTextureAlphaMod(mTexture, alpha);
 }
 
-void LTexture::render(int x, int y, SDL_Rect* clip){
+void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip){
     SDL_Rect renderQuad = {x, y, mWidth, mHeight};
     if (clip != NULL)
     {
         renderQuad.w = clip->w;
         renderQuad.h = clip->h;
     }
-    SDL_RenderCopy(sdlRenderer, mTexture, clip, &renderQuad);
+    SDL_RenderCopyEx(sdlRenderer, mTexture, clip, &renderQuad, angle, center, flip);
 }
 
 int LTexture::getHeight(){
@@ -119,6 +146,7 @@ const int WALKING_ANIMATION_FRAMES = 4;
 SDL_Rect gSpriteClips[WALKING_ANIMATION_FRAMES];
 LTexture gTexture;
 LTexture gBackgroundTexture;
+LTexture gTextTexture;
 
 bool init(){
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -143,38 +171,39 @@ bool init(){
         printf_s("SLD image could not be initialized. error:%s\n", IMG_GetError());
         return false;
     }
+    if (TTF_Init() == -1)
+    {
+        printf_s("TTF could not initialize, Error %s\n",TTF_GetError());
+        return false;
+    }
     return true;
 }
 
 bool loadMedia(){
     bool success = true;
 
-    if (!gTexture.loadFromFile("sprites.png"))
+    gFont = TTF_OpenFont("OpenSans-Regular.ttf", 72);
+    if (gFont == NULL)
+    {
+        printf_s("Failed to load font, Error %s\n", TTF_GetError());
+        success = false;
+    } else {
+        SDL_Color textColor = {0,0,0};
+        if (!gTextTexture.loadFromRenderedText("DIT IS EEN STUK TEXT", textColor)){
+            printf_s("Failed to render text texture\n");
+            success = false;
+        }
+    }
+
+
+    if (!gTexture.loadFromFile("hello2.png"))
     {
         printf_s("failed load sprites");
         success = false;
     } else {
         gTexture.setBlendMode(SDL_BLENDMODE_BLEND);
 
-        gSpriteClips[ 0 ].x =   0;
-        gSpriteClips[ 0 ].y =   0;
-        gSpriteClips[ 0 ].w =  64;
-        gSpriteClips[ 0 ].h = 205;
-
-        gSpriteClips[ 1 ].x =  64;
-        gSpriteClips[ 1 ].y =   0;
-        gSpriteClips[ 1 ].w =  64;
-        gSpriteClips[ 1 ].h = 205;
         
-        gSpriteClips[ 2 ].x = 128;
-        gSpriteClips[ 2 ].y =   0;
-        gSpriteClips[ 2 ].w =  64;
-        gSpriteClips[ 2 ].h = 205;
-
-        gSpriteClips[ 3 ].x = 196;
-        gSpriteClips[ 3 ].y =   0;
-        gSpriteClips[ 3 ].w =  64;
-        gSpriteClips[ 3 ].h = 205;
     }
 
     return success;
@@ -183,10 +212,14 @@ bool loadMedia(){
 void close(){
     gBackgroundTexture.free();
     gTexture.free();
+    gTextTexture.free();
+    TTF_CloseFont(gFont);
+    gFont = NULL;
     SDL_DestroyRenderer(sdlRenderer);
     SDL_DestroyWindow(screen);
     sdlRenderer = NULL;
     screen = NULL;
+    TTF_Quit();
     SDL_Quit();
     IMG_Quit();
 }
@@ -217,6 +250,8 @@ int main(int argc, char* args[])
     }
 
     int frame = 0;
+    double degrees = 0;
+        SDL_RendererFlip flipType = SDL_FLIP_NONE;
 
     while (1){
         ++frame;
@@ -225,6 +260,8 @@ int main(int argc, char* args[])
             frame = 0;
         }
 
+        
+         
         SDL_Event e;
         if (SDL_PollEvent(&e))
         {
@@ -233,13 +270,39 @@ int main(int argc, char* args[])
                 break;
             } else if (e.type == SDL_KEYDOWN)
             {
+                switch (e.key.keysym.sym)
+                {
+                    case SDLK_a:
+                    degrees -= 60;
+                    break;
+                    case SDLK_d:
+                    degrees += 60;
+                    case SDLK_q:
+                    flipType = SDL_FLIP_HORIZONTAL;
+                    break;
+                    case SDLK_w:
+                    flipType = SDL_FLIP_NONE;
+                    break;
+                    case SDLK_e:
+                    flipType = SDL_FLIP_VERTICAL;
+                    break;
+
+                default:
+
+                    break;
+                }
             }
         }
         SDL_SetRenderDrawColor(sdlRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(sdlRenderer);
         
-        SDL_Rect* currentClip = &gSpriteClips[frame / 4];
-        gTexture.render((SCREEN_WIDTH - currentClip->w) / 2, (SCREEN_HEIGHT - currentClip->h)/2, currentClip);
+        //SDL_Rect* currentClip = &gSpriteClips[frame / 4];
+        //gTexture.render((SCREEN_WIDTH - currentClip->w) / 2, (SCREEN_HEIGHT - currentClip->h)/2, currentClip);
+
+
+        gTexture.render(0,0, NULL, degrees, NULL, flipType);
+
+        gTextTexture.render(100,100);
 
         SDL_RenderPresent(sdlRenderer);
     }
